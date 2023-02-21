abbab5730fa69be60c20c5bfbcbb79f4b8e2d264
tannerwoodrum <tannerwoodrum@TANNERWOODR8C05>
2023-02-21 10:36:06 -0700

    Add project files.

---
+++ Mission08_Team(0105)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mission08_Team_0105_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
